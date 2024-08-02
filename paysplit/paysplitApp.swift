66f182732c8c243165a11e5d6c07d322f86db475
Ing. Ebu Celik <ebucelik1@hotmail.com>
2024-08-02 12:15:17 +0200

    Initial Commit

---
+++ paysplit/paysplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  paysplitApp.swift
+//  paysplit
+//
+//  Created by Ing. Ebu Bekir Celik, BSc, MSc on 02.08.24.
+//
+
+import SwiftUI
+
+@main
+struct paysplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
