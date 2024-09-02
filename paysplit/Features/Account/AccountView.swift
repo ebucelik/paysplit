eb817fc4df5d19d04f595ae44c35de3da6d54c54
Ing. Ebu Celik <ebucelik1@hotmail.com>
2024-09-02 17:52:53 +0200

    account details added

--- paysplit/Features/Account/AccountView.swift
+++ paysplit/Features/Account/AccountView.swift
@@ -14,48 +14,80 @@ struct AccountView: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack {
-                    HStack {
-                        GeometryReader { proxy in
-                            ZStack {
-                                Color
-                                    .app(.primary)
-                                    .clipShape(
-                                        RoundedRectangle(cornerRadius: 8)
-                                    )
+            VStack {
+                if case let .loaded(account) = store.accountState {
+                    ScrollView {
+                        VStack {
+                            HStack {
+                                GeometryReader { proxy in
+                                    ZStack {
+                                        Color
+                                            .app(.primary)
+                                            .clipShape(
+                                                RoundedRectangle(cornerRadius: 8)
+                                            )
 
-                                Image(systemName: "person.circle.fill")
-                                    .resizable()
-                                    .frame(width: 100, height: 100)
-                                    .foregroundStyle(.gray)
-                                    .position(
-                                        x: 80,
-                                        y: (proxy.size.height / 2) + 40
-                                    )
+                                        Image(systemName: "person.circle.fill")
+                                            .resizable()
+                                            .frame(width: 100, height: 100)
+                                            .foregroundStyle(.gray)
+                                            .overlay {
+                                                Circle()
+                                                    .stroke(.white, lineWidth: 1)
+                                            }
+                                            .position(
+                                                x: 80,
+                                                y: (proxy.size.height / 2) + 40
+                                            )
 
-                                Image("appIconNoBackground")
-                                    .resizable()
-                                    .frame(width: 40, height: 40)
-                                    .background(Color.white)
-                                    .clipShape(
-                                        .rect(
-                                            topLeadingRadius: 8,
-                                            bottomTrailingRadius: 8
+                                        VStack(alignment: .leading) {
+                                            Text("\(account.firstname) \(account.lastname)")
+                                                .font(.app(.title2(.bold)))
+                                                .foregroundStyle(Color.app(.secondary))
+
+                                            Text("@\(account.username)")
+                                                .font(.app(.body1(.regular)))
+                                                .foregroundStyle(Color.app(.secondary))
+                                        }
+                                        .position(
+                                            x: (proxy.size.width / 2) + 20,
+                                            y: (proxy.size.height / 2) + 20
                                         )
-                                    )
-                                    .position(
-                                        x: proxy.size.width - 19,
-                                        y: proxy.size.height - 19
-                                    )
+                                    }
+                                }
+                                .frame(height: 100)
                             }
-                        }
-                        .frame(height: 100)
-                    }
+                            .padding(.bottom, 40)
 
-                    Spacer()
+                            Divider()
+
+                            Button {
+                                print("logout")
+                            } label: {
+                                Text("Logout")
+                                    .font(.app(.body(.regular)))
+                                    .foregroundStyle(Color.app(.primary))
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                            }
+                            .padding(.vertical, 4)
+
+                            Divider()
+
+                            Button {
+                                print("delete")
+                            } label: {
+                                Text("Delete Account")
+                                    .font(.app(.body(.regular)))
+                                    .foregroundStyle(Color.app(.error))
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                            }
+                            .padding(.vertical, 4)
+
+                            Divider()
+                        }
+                        .padding()
+                    }
                 }
-                .padding()
             }
             .navigationTitle("Account")
             .navigationBarTitleDisplayMode(.inline)
